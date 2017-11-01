85df9380caa8c9a4629fd908427755318173f366
VictorHdzF <victor_hferzuli@hotmail.com>
2017-11-01 09:39:02 -0600

    ...

---
+++ app/src/main/java/mx/itesm/csf/indoorapp/zoneInfo.java
@@ -0,0 +1,13 @@
+package mx.itesm.csf.indoorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zoneInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zone_info);
+    }
+}
